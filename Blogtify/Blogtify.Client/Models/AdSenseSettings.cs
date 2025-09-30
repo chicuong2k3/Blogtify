14aba9aed0162ca011b76500e69460bb3b8b2a87
chicuong2k3 <nguyenchicuongk21@gmail.com>
2025-10-01 02:00:34 +0700

    update ads

---
+++ Blogtify/Blogtify.Client/Models/AdSenseSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Blogtify.Client.Models;
+
+public class AdSenseSettings
+{
+    public string ClientId { get; set; } = string.Empty;
+    public string SlotId { get; set; } = string.Empty;
+}
--- Blogtify/Blogtify.Client/Program.cs
+++ Blogtify/Blogtify.Client/Program.cs
@@ -1,10 +1,23 @@
 using Blogtify.Client;
+using Blogtify.Client.Models;
 using Blogtify.Client.Theming;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PlayVerse.Web.Client.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+using var response = await new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+}
+    .GetAsync("appsettings.json");
+
+using var stream = await response.Content.ReadAsStreamAsync();
+builder.Configuration.AddJsonStream(stream);
+
+builder.Services.Configure<AdSenseSettings>(
+    builder.Configuration.GetSection("AdSense"));
 
 builder.Services.AddScoped<IHttpContextProxy, WebAssemblyHttpContextProxy>();
 
