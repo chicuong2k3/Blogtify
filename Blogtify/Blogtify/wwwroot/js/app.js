b06b8ef6dd8fd497b9fd12c55db955164fe4b774
chicuong2k3 <nguyenchicuongk21@gmail.com>
2025-10-02 00:05:58 +0700

    update app.js

--- Blogtify/Blogtify/wwwroot/js/app.js
+++ Blogtify/Blogtify/wwwroot/js/app.js
@@ -1,23 +1,14 @@
-﻿(function () {
-    if (window.location.hostname !== "localhost") {
-        console.log = function () { };
-        console.debug = function () { };
-        console.info = function () { };
-        console.warn = function () { };
-        console.error = function () { };
-    }
-})();
-
+﻿
 window.MathJax = {
     options: {
-        enableExplorer: false,     
-        a11y: { speech: false }   
+        enableExplorer: false,
+        a11y: { speech: false }
     },
     loader: {
-        load: ['input/tex', 'output/chtml']  
+        load: ['input/tex', 'output/chtml']
     },
     tex: {
-        inlineMath: [['$', '$'], ['\\(', '\\)']] 
+        inlineMath: [['$', '$'], ['\\(', '\\)']]
     },
     chtml: {
         fontURL: 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/output/chtml/fonts/woff-v2'
