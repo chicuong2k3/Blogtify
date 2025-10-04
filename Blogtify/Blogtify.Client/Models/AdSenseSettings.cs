ab5c21bc06eddc4ae5a81594bc66ff618c83683f
chicuong2k3 <nguyenchicuongk21@gmail.com>
2025-10-04 16:26:56 +0700

    update ads

--- Blogtify/Blogtify.Client/Models/AdSenseSettings.cs
+++ Blogtify/Blogtify.Client/Models/AdSenseSettings.cs
@@ -4,4 +4,5 @@ public class AdSenseSettings
 {
     public string ClientId { get; set; } = string.Empty;
     public string SlotId { get; set; } = string.Empty;
+    public string LayoutKey { get; set; } = string.Empty;
 }
