e321d66914005536f2237e742cc2e5dbf442336a
chicuong2k3 <nguyenchicuongk21@gmail.com>
2025-10-05 13:41:58 +0700

    use webp images

---
+++ Blogtify/ConvertToWebP/Program.cs
@@ -0,0 +1,13 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Webp;
+using System.IO;
+
+var files = Directory.GetFiles("content", "*.*", SearchOption.AllDirectories)
+    .Where(f => f.EndsWith(".png") || f.EndsWith(".jpg") || f.EndsWith(".jpeg") || f.EndsWith(".gif"));
+
+foreach (var file in files)
+{
+    using var image = SixLabors.ImageSharp.Image.Load(file);
+    var output = Path.Combine("wwwroot", Path.GetFileNameWithoutExtension(file) + ".webp");
+    image.Save(output, new WebpEncoder());
+}
