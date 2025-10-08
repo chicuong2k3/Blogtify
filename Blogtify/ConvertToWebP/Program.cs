2879c57800f2c4c37fe19d4e5ccebc6841b65e33
chicuong2k3 <nguyenchicuongk21@gmail.com>
2025-10-09 00:26:01 +0700

    fix file convert

--- Blogtify/ConvertToWebP/Program.cs
+++ Blogtify/ConvertToWebP/Program.cs
@@ -4,17 +4,26 @@ using System.IO;
 
 string projectRoot = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "Blogtify.Client"));
 string contentPath = Path.Combine(projectRoot, "content");
+string outputFolder = Path.Combine(projectRoot, "wwwroot");
+
 if (!Directory.Exists(contentPath))
 {
     Console.WriteLine($"Folder not found: {contentPath}");
     return;
 }
 
-var files = Directory.GetFiles(contentPath, "*.*", SearchOption.AllDirectories);
+if (!Directory.Exists(outputFolder))
+{
+    Directory.CreateDirectory(outputFolder);
+}
+
+var files = Directory.GetFiles(contentPath, "*.*", SearchOption.AllDirectories)
+                .Where(f => new[] { ".png", ".jpg", ".jpeg", ".bmp", ".gif", ".webp" }
+                .Contains(Path.GetExtension(f).ToLower()));
 
 foreach (var file in files)
 {
     using var image = SixLabors.ImageSharp.Image.Load(file);
-    var output = Path.Combine("wwwroot", Path.GetFileNameWithoutExtension(file) + ".webp");
+    var output = Path.Combine(outputFolder, Path.GetFileNameWithoutExtension(file) + ".webp");
     image.Save(output, new WebpEncoder());
 }
